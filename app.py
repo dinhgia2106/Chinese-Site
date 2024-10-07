cb012508a5f15344115eeec2f28da3b592c45047
dinhgia2106 <dinhgia2106@gmail.com>
2024-10-07 20:23:42 +0700

    ignore ssl

--- app.py
+++ app.py
@@ -21,7 +21,16 @@ import requests
 from time import time
 import base64
 import re
+import ssl
 
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
 load_dotenv()  # Load biến môi trường từ .env
 
 app = Flask(__name__)
